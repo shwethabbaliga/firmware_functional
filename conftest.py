ee40401c661c40a1ee124af046b9295800585a4e
shwethabbaliga <shwethab.baliga@gmail.com>
2026-06-19 09:33:54 +0530

    changes

--- conftest.py
+++ conftest.py
@@ -4,8 +4,8 @@ from pathlib import Path
 import os
 
 ROOT_FOLDER = Path(__file__).parent
-NEW_FW = ROOT_FOLDER / new_firmware
-LOGS = ROOT_FOLDER / logs 
+NEW_FW = ROOT_FOLDER / "new_firmware"
+LOGS = ROOT_FOLDER / "logs"
 
 @pytest.fixture(scope="session",autouse=True)
 def project_setting():  
